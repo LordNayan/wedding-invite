fcf5a2e300464340245065863654640b77489a1d
Nayan Lakhwani <nayan.lakhwani@convosight.com>
2026-06-25 01:04:08 +0530

    feat: addog image

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   metadataBase: new URL(
-    process.env.NEXT_PUBLIC_SITE_URL ?? "https://example.com"
+    process.env.NEXT_PUBLIC_SITE_URL ?? "https://wedding-invite-iqtn.vercel.app"
   ),
   title: siteConfig.meta.title,
   description: siteConfig.meta.description,
